3869f5e59a8cda791bea0a485b7f0e47ebaf716b
Julian Juno <jouleyan91@hotmail.com>
2024-10-06 16:50:35 +0200

    First interactive js return of booklist

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,24 @@
+class Library {
+    constructor() {
+        this.books = []
+    }
+
+    addBookToLibrary(book) {
+        this.books.push(book)
+        this.renderBooks(); // Call to update the display whenever a book is added
+    }
+
+    listBooks() {
+        return this.books.map(book => book.info()).join('<br>'); // Use <br> for HTML line breaks
+    }
+
+    renderBooks() {
+        const bookListDiv = document.getElementById("book-list");
+        bookListDiv.innerHTML = this.listBooks(); // Update the inner HTML of the div
+    }
+} 
+
+
 class Book {
     constructor(title, author, numPages, read) {
         this.title = title;
@@ -13,6 +34,11 @@ class Book {
     }
 }
 
+// NE JAMAIS DECLARER LES VARIABLES AVANT L'ENTIERE DEFINITION DES FONCTIONS (VOIR "DOM")
+/*
 // Example usage:
+const myLibary = new Library();
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 console.log(theHobbit.info());
+
+*/
