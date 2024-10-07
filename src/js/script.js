35c356572ba24031a9da73c4241abda9a9db30cd
Julian Juno <jouleyan91@hotmail.com>
2024-10-07 13:12:46 +0200

    Table filled properly

--- src/js/script.js
+++ src/js/script.js
@@ -25,6 +25,7 @@ export class Library {
         if (book instanceof Book) { // Check if the passed object is an instance of Book
             this.books.push(book);
             this.renderBooks(); // Automatically render after adding a book !!! A chaque fois qu'on ajoute un livre à la bibliothèque, on met à jour la bibliothèque affichée !
+            this.renderBooksInTable();
         } else {
             console.error('Invalid book object');
         }
@@ -39,5 +40,24 @@ export class Library {
         bookListDiv.innerHTML = this.listBooks(); 
         // Update the inner HTML of the div, injection de l'HTML. Lorsqu'une méthode modifie le DOM (Document Object Model), il est courant qu'elle ne retourne pas de valeur. 
         // Les méthodes qui ont des effets secondaires (comme changer le contenu d'une page web) ne nécessitent généralement pas de retour de valeur.
+    }
+
+    renderBooksInTable() {
+        const bookTableBody = document.getElementById('book-table-body'); // Get the tbody element
+        // Clear existing rows if necessary --> OUI PLUS QUE N2CESSAIRE (VOIR SCREENSHOT !)
+        bookTableBody.innerHTML = '';
+        
+        // Loop through the library's books and create table rows
+        this.books.forEach(book => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${book.title}</td>
+                <td>${book.author}</td>
+                <td>${book.numPages}</td>
+                <td>${book.read ? 'Yes' : 'No'}</td>
+            `;
+            // Append the new row to the tbody
+        bookTableBody.appendChild(row);
+    });
     }
 }
